56cf80e4852dfdd74c7ec6725e4a6a0a7b715043
Colin Mullikin <CMullikin@gohealth.com>
2017-10-17 17:58:44 -0500

    Gradle set-up and basic SpringApplication class

---
+++ src/main/java/com/gohealth/SpringToDo/Application.java
@@ -0,0 +1,16 @@
+package com.gohealth.SpringToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main SpringApplication.
+ * @author <a href="mailto:cmullikin@gohealth.com">Colin Mullikin</a>
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
